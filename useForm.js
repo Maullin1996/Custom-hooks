b88ae34cf3ad0e2bf524df88e55b958850974345
Mauricio <llanosmauricio10@gmail.com>
2023-10-27 22:10:29 -0500

    Primer commit

---
+++ useForm.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export const useForm = ( initialForm = {} ) => {
+
+    const [formState, setFormState] = useState(initialForm);
+
+    //const { username, email, password } = formState
+
+    const onImputChange = ({target}) => {
+
+        const { name, value } = target;
+        setFormState({
+            ...formState,
+            [ name ]: value
+        });
+
+    }
+
+    const onResetform = () =>{
+
+        setFormState(initialForm);
+    }
+
+
+    return {
+        ...formState,
+        formState,
+        onImputChange,
+        onResetform,
+
+    }
+}
